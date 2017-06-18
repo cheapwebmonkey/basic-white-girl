a9c3fd2c88a4149de3d6910aa756261245b454c3
Margeaux Spring <contact@cheapwebmonkey.com>
2017-06-18 14:00:43 -0400

    inital commit

---
+++ README.md
@@ -0,0 +1,6 @@
+This is my Basic White Girl Alexa skill in node.js, forked from the Alexa github example repo.
+Alpha version, will eventually make an api call to fetch the json answers.
+
+Pull requests welcome for facts.
+
+Thank you and like, please, drive thru.
---
+++ src/index.js
@@ -0,0 +1,55 @@
+'use strict';
+var Alexa = require('alexa-sdk');
+
+var APP_ID = "amzn1.ask.skill.971bb2c0-6dad-4c73-9a24-c1d59a98dd48";
+
+var SKILL_NAME = "Basic White Girl Facts";
+var GET_FACT_MESSAGE = "Like, here's your fact or whatevs: ";
+var HELP_MESSAGE = "So like, you can totes say tell me a basic white girl fact, or, you can say exit or whatever... Like, what can I help you with, or something?";
+var HELP_REPROMPT = "O.M.G., Seriously, like, what can I help you with?";
+var STOP_MESSAGE = "As if! Goodbye!";
+
+var data = [
+    "Like, if your name is Becky, you're probs a Basic White Girl. No worries.",
+    "I was like, at my fave Starbucks and they were out of Pumpkin Spiced fraps and I was like, O.M.G!, how is life?",
+    "So, if you like put glitter of, ummmm, any kind on a white girl, she is, like, magically transformed into a Basic White Girl, or whatever.",
+    "If you own literally eleventy-million pairs of Uggs, like you're def a Basic White Girl.",
+    "It's not Instagram. It's Insta. As if.",
+    "Basic White Girls literally will not date someone who isn't at least a 7. Because Insta.",
+    "So, like, basic white girls think they are totes unique and special and trendsetting. Literally.",
+    "Ummmm, Basic White Girls totes take like, seventy five milllion selfies before they find the perf one to Insta.",
+    "Quoting Mean Girls like, automagically confers Basic White Girl status onto a reg white girl, mos def.",
+    "Do you totes love Lisa Frank? Then you're totes a Basic White Girl."
+];
+
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+var handlers = {
+    'LaunchRequest': function () {
+        this.emit('GetNewFactIntent');
+    },
+    'GetNewFactIntent': function () {
+        var factArr = data;
+        var factIndex = Math.floor(Math.random() * factArr.length);
+        var randomFact = factArr[factIndex];
+        var speechOutput = GET_FACT_MESSAGE + randomFact;
+        this.emit(':tellWithCard', speechOutput, SKILL_NAME, randomFact)
+    },
+    'AMAZON.HelpIntent': function () {
+        var speechOutput = HELP_MESSAGE;
+        var reprompt = HELP_REPROMPT;
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', STOP_MESSAGE);
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', STOP_MESSAGE);
+    }
+};
